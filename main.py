05d8635cc872b0dfe19ead990b92878ef8a49cc6
むずでょ <muzudho1@gmail.com>
2024-09-13 23:29:22 +0900

    自動算出

--- main.py
+++ main.py
@@ -118,73 +118,69 @@ if __name__ == '__main__':
     """コマンドから実行時"""
 
     try:
-        round_count = 2_000_000
 
         # 0.50 ～ 0.99 まで試算
         rule_list = [
-            # black_win_rate, bout_count, white_require
+            # black_win_rate, best_bout_count, best_white_require
             # -----------------------------------------
-            # round_letro(1/(1-black_win_rate)-1)
-            [0.50,  1,  1], # 0.5:0.5
-            [0.51,  1,  1],
-            [0.52,  1,  1],
-            [0.53,  1,  1],
-            [0.54,  1,  1],
-            [0.55,  1,  1],
-            [0.56,  1,  1],
-            [0.57,  1,  1],
-            [0.58,  1,  1],
-            [0.59,  1,  1],
-
-            # 60％で既存式では 2-bout になるが、調整黒番勝率が 35.9915 ％ に下がってしまう。調整を入れる
-            [0.60,  2+2,  1+2], # 1:1=60.0%, 2:1=35.9%, 3:2=64.8%, 4:3=
-            [0.61,  2+2,  1+2],
-            [0.62,  2+2,  1+2],
-            [0.63,  2+2,  1+2],
-            [0.64,  2+2,  1+2],
-            [0.65,  2+2,  1+2],
-            # 0.65 まで調整
-
-            [0.66,  2,  1],
-            [0.67,  2,  1],
-            [0.68,  2,  1],
-            [0.69,  2,  1],
-            [0.70,  2,  1],
-            [0.71,  2,  1],
-            [0.72,  3,  2],
-            [0.73,  3,  2],
-            [0.74,  3,  2],
-            [0.75,  3,  2],
-            [0.76,  3,  2],
-            [0.77,  3,  2],
-            [0.78,  4,  3],
-            [0.79,  4,  3],
-            [0.80,  4,  3],
-            [0.81,  4,  3],
-            [0.82,  5,  4],
-            [0.83,  5,  4],
-            [0.84,  5,  4],
-            [0.85,  6,  5],
-            [0.86,  6,  5],
-            [0.87,  7,  6],
-            [0.88,  7,  6],
-            [0.89,  8,  7],
-            [0.90,  9,  8],
-            [0.91, 10,  9],
-            [0.92, 12, 11],
-            [0.93, 13, 12],
-            [0.94, 16, 15],
-            [0.95, 19, 18],
-            [0.96, 24, 23],
-            [0.97, 32, 31],
-            [0.98, 49, 48],
-            [0.99, 99, 98],
+            # 人の目で見て十分だと思ったら、best_bout_count と best_white_require を 0 以外にすること
+            [0.50,  0,  0],
+            [0.51,  0,  0],
+            [0.52,  0,  0],
+            [0.53,  0,  0],
+            [0.54,  0,  0],
+            [0.55,  0,  0],
+            [0.56,  0,  0],
+            [0.57,  0,  0],
+            [0.58,  0,  0],
+            [0.59,  0,  0],
+            [0.60,  0,  0],
+            [0.61,  0,  0],
+            [0.62,  0,  0],
+            [0.63,  0,  0],
+            [0.64,  0,  0],
+            [0.65,  0,  0],
+            [0.66,  0,  0],
+            [0.67,  0,  0],
+            [0.68,  0,  0],
+            [0.69,  0,  0],
+            [0.70,  0,  0],
+            [0.71,  0,  0],
+            [0.72,  0,  0],
+            [0.73,  0,  0],
+            [0.74,  0,  0],
+            [0.75,  0,  0],
+            [0.76,  0,  0],
+            [0.77,  0,  0],
+            [0.78,  0,  0],
+            [0.79,  0,  0],
+            [0.80,  0,  0],
+            [0.81,  0,  0],
+            [0.82,  0,  0],
+            [0.83,  0,  0],
+            [0.84,  0,  0],
+            [0.85,  0,  0],
+            [0.86,  0,  0],
+            [0.87,  0,  0],
+            [0.88,  0,  0],
+            [0.89,  0,  0],
+            [0.90,  0,  0],
+            [0.91,  0,  0],
+            [0.92,  0,  0],
+            [0.93,  0,  0],
+            [0.94,  0,  0],
+            [0.95,  0,  0],
+            [0.96,  0,  0],
+            [0.97,  0,  0],
+            [0.98,  0,  0],
+            [0.99,  0,  0],
         ]
 
         for rule in rule_list:
             black_win_rate=rule[0]
-            bout_count=rule[1]
-            white_require=rule[2]
+            best_bout_count=rule[1]
+            best_white_require=rule[2]
+            is_automatic = best_bout_count == 0 or best_white_require == 0
 
             # （仮説）何本勝負にするかは、以下の式で求まる
             # bout_count = round_letro(1/(1-black_win_rate)-1)
@@ -194,14 +190,90 @@ if __name__ == '__main__':
             #bout_count = math.ceil(1/(1-black_win_rate)-1)
             #print(f"試算： 1 / ( 1 - {black_win_rate} ) - 1 = {bout_count} ※小数点以下切り上げ")
 
-            black_win_count = n_round(
-                black_win_rate=black_win_rate,
-                bout_count=bout_count,
-                white_require=white_require,
-                round_count=round_count)
+            # 途中の計算式
+            calculation_list = []
+
+            # best_bout_count と best_white_require が未設定なら、アルゴリズムで求めることにする
+            if is_automatic:
+                best_black_win_count = 0
+                best_bout_count = 0
+                best_white_require = 0
+                round_count = 20_000
+
+                # 誤差は 0.01 に接近するほどベスト。勝率は最低で 0.0、最大で 1.0 なので、0.5 との誤差は 0.5 が最大
+                # 0.01 未満からさらに 0 に近づいていくので、そうなる前に打ち切る
+                LIMIT = 0.01
+                OUT_OF_ERROR = 0.51
+                best_black_win_error = OUT_OF_ERROR
+
+                is_cutoff = False
+
+                for bout_count in range(1, 10):
+
+                    # １本勝負のときだけ、白はｎ本－１ではない
+                    if bout_count == 1:
+                        end_white_require = 2
+                    else:
+                        end_white_require = bout_count
+
+                    for white_require in range(1, end_white_require):
+
+                        black_win_count = n_round(
+                            black_win_rate=black_win_rate,
+                            bout_count=bout_count,
+                            white_require=white_require,
+                            round_count=round_count)
+                        
+                        #print(f"{black_win_count=}  {round_count=}  {black_win_count / round_count=}")
+                        black_win_error = abs(black_win_count / round_count - 0.5)
+
+                        if best_black_win_error != OUT_OF_ERROR and black_win_error < LIMIT:
+                            is_cutoff = True
+
+                            # 進捗バー
+                            print('x', end='')
+
+                            break
+
+                        if black_win_error < best_black_win_error:
+                            best_black_win_count = black_win_count
+                            best_black_win_error = black_win_error
+                            best_bout_count = bout_count
+                            best_white_require = white_require
+                        
+                            # 進捗バー（更新時）
+                            text = f'[{black_win_error:6.4f}]'
+                            calculation_list.append(text)
+                            print(text, end='')
+
+                    if is_cutoff:
+                        break
+
+                    # 進捗バー
+                    print('.', end='')
+                print() # 改行
+
+            # best_bout_count と best_white_require が設定されていれば、より細かく確率を求める
+            else:
+                round_count = 2_000_000
+
+                best_black_win_count = n_round(
+                    black_win_rate=black_win_rate,
+                    bout_count=best_bout_count,
+                    white_require=best_white_require,
+                    round_count=round_count)
+
+                best_black_win_error = abs(best_black_win_count / round_count - 0.5)
+
 
             with open('result_summary.log', 'a', encoding='utf8') as f:
-                text = f"先手勝率：{black_win_rate:4.02f}  {bout_count:2}本勝負×{round_count}回  調整先手勝率：{black_win_count * 100 / round_count:7.04f} ％\n"
+                # 計算未完了
+                if best_black_win_error == OUT_OF_ERROR:
+                    text = f"先手勝率：{black_win_rate:4.02f}  （計算未完了）  {is_automatic=}  {''.join(calculation_list)}\n"
+                
+                # 計算可能
+                else:
+                    text = f"先手勝率：{black_win_rate:4.02f}  {best_bout_count:2}本勝負×{round_count}回  白{best_white_require:2}先取制  調整先手勝率：{best_black_win_count * 100 / round_count:>7.04f} ％  {is_automatic=}  {''.join(calculation_list)}\n"
 
                 f.write(text)
                 print(text, end='')
