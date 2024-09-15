db44c1ffe442fe8d9d9236fc7fadacf01f995d27
むずでょ <muzudho1@gmail.com>
2024-09-16 03:23:32 +0900

    ライブラリ化準備

--- main_coin_toss.py
+++ main_coin_toss.py
@@ -1,6 +1,6 @@
 #
 # シミュレーション
-# python main_toss.py
+# python main_coin_toss.py
 #
 #   表の出る確率（black_win_rate）が偏ったコインを、指定回数（max_bout_count）投げる
 #
@@ -13,39 +13,41 @@ import math
 from library import BLACK, WHITE, coin, n_bout, n_round, round_letro
 
 
-SUMMARY_FILE_PATH = 'simple_test.log'
-
-# 誤差は LIMIT に接近するほどベスト。勝率は最低で 0.0、最大で 1.0 なので、0.5 との誤差は 0.5 が最大
-# LIMIT 未満からさらに 0 に近づいていくので、そうなる前に打ち切る
-LIMIT = 0.01   # 例えば LIMIT = 0.03 にすると、黒番勝率 0.53 のときに 0.5 へ近づけるため 0.03 縮める必要があるから、運悪く 1:1 のときに外すと、そのあと見つけるのに時間がかかるようだ
-OUT_OF_ERROR = 0.51
+SUMMARY_FILE_PATH = 'main_coin_toss.log'
 
 
-########################################
-# コマンドから実行時
-########################################
+class CoinToss():
+    """コイントスの試行"""
 
 
-if __name__ == '__main__':
-    """コマンドから実行時"""
+    def __init__(self, summary_file_path):
+        """初期化
+        
+        Parameters
+        ----------
+        summary_file_path : str
+            結果の出力ファイルへのパス
+        """
+        self._summary_file_path = summary_file_path
 
-    try:
+
+    def coin_toss_in_some_rounds(self, black_win_rate, black_target_in_bout, white_target_in_bout, round_total):
+        """コイントスを複数対局する
+        
+        Parameters
+        ----------
+        black_win_rate : float
+            黒が出る確率（先手勝率）
+        black_target_in_bout : int
+            先手の何本先取制
+        white_target_in_bout : int
+            後手の何本先取制
+        round_total : int
+            対局数
+        """
 
         # 初期値
         # ------
-
-        # 先手勝率
-        black_win_rate=0.80
-
-        # 先手の何本先取制
-        black_target_in_bout = 4
-        #black_target_in_bout = 3   # 先手勝率80% なら、黒の４本先取より、黒の３本先取の方が勝率５割に近い
-
-        # 後手の何本先取制
-        white_target_in_bout = 1
-
-        # 対局数
-        round_total = 2_000_000
 
         # 黒が勝った回数
         black_wons = 0
@@ -78,7 +80,7 @@ if __name__ == '__main__':
                     break
 
 
-        with open(SUMMARY_FILE_PATH, 'a', encoding='utf8') as f:
+        with open(self._summary_file_path, 'a', encoding='utf8') as f:
             # 文言作成
             # -------
 
@@ -89,6 +91,27 @@ if __name__ == '__main__':
             print(text) # 表示
             f.write(f"{text}\n")    # ファイルへ出力
 
+
+########################################
+# コマンドから実行時
+########################################
+
+
+if __name__ == '__main__':
+    """コマンドから実行時"""
+
+    try:
+        CoinToss(
+            summary_file_path=SUMMARY_FILE_PATH).coin_toss_in_some_rounds(
+                # 先手勝率
+                black_win_rate=0.65,
+                # 先手の何本先取制
+                black_target_in_bout=9,
+                # 後手の何本先取制
+                white_target_in_bout=5,
+                # 対局数
+                round_total=2_000_000)
+            #black_target_in_bout = 3   # NOTE 先手勝率80% なら、黒の４本先取より、黒の３本先取の方が勝率５割に近い
 
     except Exception as err:
         print(f"[unexpected error] {err=}  {type(err)=}")
