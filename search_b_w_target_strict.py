f4837fc9563c2afac6c844390573065c3a2108e8
むずでょ <muzudho1@gmail.com>
2024-09-18 02:58:55 +0900

    整形

--- search_b_w_target_strict.py
+++ search_b_w_target_strict.py
@@ -144,11 +144,13 @@ if __name__ == '__main__':
                 # 後手がアドバンテージを持っているという表記に変更
                 w_advantage = best_b_point - best_w_point
 
-                text = f"[{datetime.datetime.now()}]  先手勝率 {black_win_rate*100:2.0f} ％ --調整--> {best_balanced_black_win_rate*100:6.4f} ％ （± {best_error*100:>7.4f}）  {max_bout_count:>3}本勝負（後手は最初から {w_advantage:>2} 本持つアドバンテージ）"
+                text = ""
+                #text += f"[{datetime.datetime.now()}]  "    # タイムスタンプ
+                text += f"先手勝率 {black_win_rate*100:2.0f} ％ --調整--> {best_balanced_black_win_rate*100:6.4f} ％ （± {best_error*100:>7.4f}）  {max_bout_count:>2}本勝負（後手は最初から {w_advantage:>2} 本持つアドバンテージ）"
                 print(text) # 表示
 
-                # 計算過程を追加する場合
-                text += f"  {''.join(process_list)}"
+                # # 計算過程を追加する場合
+                # text += f"  {''.join(process_list)}"
 
                 f.write(f"{text}\n")    # ファイルへ出力
 
