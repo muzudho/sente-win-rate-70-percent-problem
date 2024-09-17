1eb808b0e066732d00813a796bc52c843b848ae0
むずでょ <muzudho1@gmail.com>
2024-09-18 00:25:42 +0900

    調べもの

--- search_b_w_target_strict.py
+++ search_b_w_target_strict.py
@@ -98,7 +98,8 @@ if __name__ == '__main__':
                 
                 #for w_point in range (1, b_point):
                 #for w_point in range (1, 2): # 後手に必要な先取本数を 1 に固定する場合
-                for w_point in range (1, 3):  # 後手に必要な先取本数を 1 ～ 2 の範囲にする場合
+                #for w_point in range (1, 3):  # 後手に必要な先取本数を 1 ～ 2 の範囲にする場合
+                for w_point in range (1, 4):  # 後手に必要な先取本数を 1 ～ 3 の範囲にする場合
 
                     if b_point <= w_point and 1 < w_point:
                         continue
