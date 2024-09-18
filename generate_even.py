7603714c3167d3186e3d327bc8401a2cbc6a101f
むずでょ <muzudho1@gmail.com>
2024-09-18 16:58:38 +0900

    pandasに保存

--- generate_even.py
+++ generate_even.py
@@ -14,7 +14,8 @@ import pandas as pd
 from library import BLACK, WHITE, coin, n_bout_without_turn, n_round_without_turn, round_letro
 
 
-SUMMARY_FILE_PATH = 'output/generate_even.log'
+LOG_FILE_PATH = 'output/generate_even_without_turn.log'
+CSV_FILE_PATH = './data/generate_even_without_turn.csv'
 
 # 先手勝率は 0.5 に近づけたい。その差の絶対値をエラーと呼んでいる。
 # LIMIT で示す値よりエラーが下回れば、もう十分なので、探索を打ち切る。打ち切らないと延々と探索してしまう
@@ -37,21 +38,16 @@ if __name__ == '__main__':
 
     try:
 
-        df = pd.read_csv("./data/generate_even_without_turn.csv", encoding="utf8")
+        df = pd.read_csv(CSV_FILE_PATH, encoding="utf8")
+        print(df)
 
-        for p, best_black_win_error, best_max_bout_count, best_round_count, best_w_point, process in zip(df['p'], df['new_p_error'], df['max_bout_count'], df['round_count'], df['w_point'], df['process']):
-            #print(f"{p=}  {best_black_win_error=}  {best_max_bout_count=}  {best_round_count=}  {best_w_point=}  {process=}  {type(process)=}")
+        for p, best_new_p, best_new_p_error, best_max_bout_count, best_round_count, best_w_point, process in zip(df['p'], df['new_p'], df['new_p_error'], df['max_bout_count'], df['round_count'], df['w_point'], df['process']):
+            #print(f"{p=}  {best_new_p_error=}  {best_max_bout_count=}  {best_round_count=}  {best_w_point=}  {process=}  {type(process)=}")
 
             # 黒の必要先取数は計算で求めます
             best_b_point = best_max_bout_count-(best_w_point-1)
 
-            is_automatic = best_black_win_error >= LIMIT or best_max_bout_count == 0 or best_round_count < 2_000_000 or best_w_point == 0
-
-            # 途中の計算式。半角空白区切り
-            if math.isnan(process):
-                process_list = []
-            else:
-                process_list = process.split(' ')
+            is_automatic = best_new_p_error >= LIMIT or best_max_bout_count == 0 or best_round_count < 2_000_000 or best_w_point == 0
 
             # アルゴリズムで求めるケース
             if is_automatic:
@@ -80,23 +76,31 @@ if __name__ == '__main__':
                             round_count=best_round_count)
                         
                         #print(f"{black_win_count=}  {best_round_count=}  {black_win_count / best_round_count=}")
-                        new_black_win_rate = black_win_count / best_round_count
-                        black_win_error = abs(new_black_win_rate - 0.5)
+                        new_p = black_win_count / best_round_count
+                        new_p_error = abs(new_p - 0.5)
 
-                        if black_win_error < best_black_win_error:
-                            best_black_win_rate = new_black_win_rate
-                            best_black_win_error = black_win_error
+                        if new_p_error < best_new_p_error:
+                            best_new_p = new_p
+                            best_new_p_error = new_p_error
                             best_max_bout_count = max_bout_count
                             best_b_point = b_point
                             best_w_point = w_point
                         
                             # 進捗バー（更新時）
-                            text = f'[{best_black_win_error:6.4f} {best_max_bout_count:2}本 {best_max_bout_count-best_w_point+1:2}黒 {best_w_point:2}白]'
+                            text = f'[{best_new_p_error:6.4f} {best_max_bout_count:2}本 {best_max_bout_count-best_w_point+1:2}黒 {best_w_point:2}白]'
                             print(text, end='', flush=True) # すぐ表示
-                            process_list.append(text)
+
+                            # process 列を更新
+                            #
+                            #   途中の計算式。半角空白区切り
+                            #
+                            if math.isnan(process):
+                                df.loc[df['p']==p, ['process']] = text
+                            else:
+                                df.loc[df['p']==p, ['process']] = f"{process} {text}"
 
                             # 十分な答えが出たので探索を打ち切ります
-                            if black_win_error < LIMIT:
+                            if best_new_p_error < LIMIT:
                                 is_cutoff = True
 
                                 # 進捗バー
@@ -109,6 +113,7 @@ if __name__ == '__main__':
 
                     # 進捗バー（ｎ本目）
                     print('.', end='', flush=True)
+
                 print() # 改行
 
             # 結果が設定されていれば、そのまま表示
@@ -116,65 +121,55 @@ if __name__ == '__main__':
                 pass
 
 
-            with open(SUMMARY_FILE_PATH, 'a', encoding='utf8') as f:
 
-                # 自動計算未完了
-                if is_automatic and best_black_win_error == OUT_OF_ERROR:
-                    text = f"先手勝率：{p*100:2} ％"                
-                else:
+            # 自動計算未完了
+            if is_automatic and best_new_p_error == OUT_OF_ERROR:
+                print(f"先手勝率：{p*100:2} ％  （自動計算未完了）")
 
-                    # DO 通分したい。最小公倍数を求める
-                    lcm = math.lcm(best_b_point, best_w_point)
-                    # 先手一本の価値
-                    b_unit = lcm / best_b_point
-                    # 後手一本の価値
-                    w_unit = lcm / best_w_point
-                    # 先手勝ち、後手勝ちの共通ゴール
-                    b_win_value_goal = best_w_point * w_unit
-                    w_win_value_goal = best_b_point * b_unit
-                    if b_win_value_goal != w_win_value_goal:
-                        raise ValueError(f"{b_win_value_goal=}  {w_win_value_goal=}")
+            else:
+                # DO 通分したい。最小公倍数を求める
+                lcm = math.lcm(best_b_point, best_w_point)
+                # 先手一本の価値
+                b_unit = lcm / best_b_point
+                # 後手一本の価値
+                w_unit = lcm / best_w_point
+                # 先手勝ち、後手勝ちの共通ゴール
+                b_win_value_goal = best_w_point * w_unit
+                w_win_value_goal = best_b_point * b_unit
+                if b_win_value_goal != w_win_value_goal:
+                    raise ValueError(f"{b_win_value_goal=}  {w_win_value_goal=}")
 
-                    text = f"先手勝率：{p*100:2.0f} ％ --調整後--> {best_black_win_rate * 100:>7.04f} ％（± {best_black_win_error * 100:>7.04f}）  {best_max_bout_count:2}本勝負×{best_round_count:6}回  先手{best_max_bout_count-best_w_point+1:2}本先取/後手{best_w_point:2}本先取制  つまり、先手一本の価値{b_unit:2.0f}  後手一本の価値{w_unit:2.0f}  ゴール{b_win_value_goal:3.0f}"
-
-
-                # 計算過程を付けずに表示
-                # --------------------
-
-                # 自動計算未完了
-                if is_automatic and best_black_win_error == OUT_OF_ERROR:
-                    print(f"{text}  （自動計算未完了）")
-
-                else:
-                    # 自動計算満了
-                    if is_automatic:
-                        print(f"{text}  （自動計算満了）")
-                    # 手動設定
-                    else:
-                        print(f"{text}  （手動設定）")
-
-
-                # 計算過程と改行を付けてファイルへ出力
-                # ---------------------------------
-
-                # 自動計算
+                print(f"先手勝率：{p*100:2.0f} ％ --調整後--> {best_new_p * 100:>7.04f} ％（± {best_new_p_error * 100:>7.04f}）  {best_max_bout_count:2}本勝負×{best_round_count:6}回  先手{best_b_point:2}本先取/後手{best_w_point:2}本先取制  つまり、先手一本の価値{b_unit:2.0f}  後手一本の価値{w_unit:2.0f}  ゴール{b_win_value_goal:3.0f}", end='')
+                # 自動計算満了
                 if is_automatic:
-                    # 計算過程
-                    text += f"  {' '.join(process_list)}"
-
-                    # 未完了
-                    if best_black_win_error == OUT_OF_ERROR:
-                        text += "  （自動計算未完了）\n"
-                    # 満了
-                    else:
-                        text += "  （自動計算満了）\n"
-
+                    print(f"  （自動計算満了）")
                 # 手動設定
                 else:
-                    text += " （手動設定）\n"
+                    print(f"  （手動設定）")
 
-                f.write(text)
+                # データフレーム更新
+                # -----------------
 
+                # ［調整後の表が出る確率］列を更新
+                df.loc[df['p']==p, ['new_p']] = best_new_p
+
+                # ［調整後の表が出る確率の５割との誤差］列を更新
+                df.loc[df['p']==p, ['new_p_error']] = best_new_p_error
+
+                # ［最大ｎ本勝負］列を更新
+                df.loc[df['p']==p, ['max_bout_count']] = best_max_bout_count
+
+                #best_b_point は max_bout_count と w_point から求まる
+
+                # ［白が勝つのに必要な先取本数］列を更新
+                df.loc[df['p']==p, ['w_point']] = best_w_point
+
+            
+            # CSV保存
+            #
+            #   NOTE なんか列が追加されるので、index=False を付けた
+            #
+            df.to_csv(CSV_FILE_PATH, index=False)
 
     except Exception as err:
         print(f"[unexpected error] {err=}  {type(err)=}")
