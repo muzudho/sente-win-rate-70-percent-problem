aaed6a090a872ee0e253e2b1d45fca1600cc88c9
むずでょ <muzudho1@gmail.com>
2024-10-04 00:03:52 +0900

    WIP 反復深化探索　作り直し中

--- create_a_csv_to_data_score_board.py
+++ create_a_csv_to_data_score_board.py
@@ -25,12 +25,14 @@ number_of_dirty = 0         # ファイルを新規作成したときに 1、レ
 INTERVAL_SECONDS_FOR_SAVE_CSV = 5   # 2
 
 
-def create_all_files(upper_limit_span):
+def create_or_update_all_files(upper_limit_span):
+
+    global number_of_dirty
 
     # ［将棋の引分け率］
     for failure_rate_percent in range(0, int(UPPER_LIMIT_FAILURE_RATE * 100) + 1, 5): # 5％刻み。 100%は除く。0除算が発生するので
         failure_rate = failure_rate_percent / 100
-        print(f"[{datetime.datetime.now()}] in create_all_files {failure_rate=:.2f}")
+        print(f"[{datetime.datetime.now()}] in create_or_update_all_files {failure_rate=:.2f}")
 
         # ［将棋の先手勝率］
         for p_percent in range(50, 96):
@@ -50,47 +52,94 @@ def create_all_files(upper_limit_span):
 
                 df, is_new = ScoreBoardDataTable.read_df(spec=spec, new_if_it_no_exists=True)
 
+
+                # FIXME 便宜的に［試行シリーズ数］は 1 固定
+                trials_series = 1
+
+
                 # ファイルが存在せず、空データフレームが新規作成されたら
                 if is_new:
-                    # FIXME 便宜的に［試行シリーズ数］は 1 固定
-                    trials_series = 1
 
-                    # ［目標の点数］
-                    for span in range(1, upper_limit_span):
-                        print(f"[{datetime.datetime.now()}] CREATE_FILE {span=}")
+                    # ループカウンター
+                    span = 1        # ［目標の点数］
+                    t_step = 1      # ［後手で勝ったときの勝ち点］
+                    h_step = 1      # ［先手で勝ったときの勝ち点］
 
-                        # ［後手で勝ったときの勝ち点］
-                        for t_step in range(1, span + 1):
-                            #print(f"[{datetime.datetime.now()}] CREATE_FILE {t_step=}")
+                    print(f"[{datetime.datetime.now()}][turn_system={turn_system_str:11}  p={spec.p:.2f}  failure_rate={spec.failure_rate:.2f}] NEW_FILE")
 
-                            # ［先手で勝ったときの勝ち点］
-                            for h_step in range(1, t_step + 1):
-                                #print(f"[{datetime.datetime.now()}] CREATE_FILE {h_step=}")
+                    # １件も処理してないが、ファイルを保存したいのでフラグを立てる
+                    number_of_dirty += 1
 
-                                # ［シリーズ・ルール］
-                                specified_series_rule = SeriesRule.make_series_rule_base(
-                                        spec=spec,
-                                        trials_series=trials_series,
-                                        h_step=h_step,
-                                        t_step=t_step,
-                                        span=span)
+                # ファイルが存在して、読み込まれたなら
+                else:
+                    # ループカウンター
+                    if len(df) < 1:
+                        span = 1        # ［目標の点数］
+                        t_step = 1      # ［後手で勝ったときの勝ち点］
+                        h_step = 1      # ［先手で勝ったときの勝ち点］
 
-                                # データフレーム更新
-                                # 新規レコード追加
-                                ScoreBoardDataTable.append_new_record(
-                                        df=df,
-                                        turn_system_str=turn_system_str,
-                                        failure_rate=spec.failure_rate,
-                                        p=spec.p,
-                                        span=span,
-                                        t_step=t_step,
-                                        h_step=h_step,
-                                        shortest_coins=specified_series_rule.shortest_coins,
-                                        upper_limit_coins=specified_series_rule.upper_limit_coins,
-                                        three_rates=None)    # NOTE three_rates を求める処理は重たいので、後回しにする
+                    else:
+                        # 途中まで処理が終わってるんだったら、途中から再開したいが。ループの途中から始められるか？
 
+                        # TODO 最後に処理された span は？
+                        span = int(df['span'].max())
+
+                        # TODO 最後に処理された span のうち、最後に処理された t_step は？
+                        t_step = int(df.loc[df['span']==span, 't_step'].max())
+
+                        # TODO 最後に処理された span, t_step のうち、最後に処理された h_step は？
+                        h_step = int(df.loc[(df['span']==span) & (df['t_step']==t_step), 'h_step'].max())
+
+                        print(f"[{datetime.datetime.now()}][turn_system={turn_system_str:11}  p={p:.2f}  failure_rate={spec.failure_rate:.2f}] RESTART_ {span=:2}  {t_step=:2}  {h_step=:2}")
+
+
+                while span < upper_limit_span + 1:
+
+                    # 該当レコードのキー
+                    key = (df['span']==span) & (df['t_step']==t_step) & (df['h_step']==h_step)
+
+                    # データが既存でないなら
+                    if not key.any():
+
+                        # ［シリーズ・ルール］
+                        specified_series_rule = SeriesRule.make_series_rule_base(
+                                spec=spec,
+                                trials_series=trials_series,
+                                h_step=h_step,
+                                t_step=t_step,
+                                span=span)
+
+                        # データフレーム更新
+                        # 新規レコード追加
+                        ScoreBoardDataTable.append_new_record(
+                                df=df,
+                                turn_system_str=turn_system_str,
+                                failure_rate=spec.failure_rate,
+                                p=spec.p,
+                                span=span,
+                                t_step=t_step,
+                                h_step=h_step,
+                                shortest_coins=specified_series_rule.shortest_coins,
+                                upper_limit_coins=specified_series_rule.upper_limit_coins,
+                                three_rates=None)    # NOTE three_rates を求める処理は重たいので、後回しにする
+                        
+                        number_of_dirty += 1
+
+
+                    # カウントアップ
+                    h_step += 1
+                    if t_step < h_step:
+                        h_step = 1
+                        t_step += 1
+                        if span < t_step:
+                            t_step = 1
+                            span += 1
+
+
+                if 0 < number_of_dirty:
                     csv_file_path_to_wrote = ScoreBoardDataTable.to_csv(df=df, spec=spec)
-                    print(f"[{datetime.datetime.now()}][turn_system={turn_system_str:11}  p={p:.2f}  failure_rate={spec.failure_rate:.2f}] CREATE_FILE  write file to `{csv_file_path_to_wrote}` ...")
+                    print(f"[{datetime.datetime.now()}][turn_system={turn_system_str:11}  p={p:.2f}  failure_rate={spec.failure_rate:.2f}] SAVE_FILE  {number_of_dirty=}  write file to `{csv_file_path_to_wrote}` ...")
+                    number_of_dirty = 0
 
 
 def update_three_rates_for_all_files(upper_limit_upper_limit_coins):
@@ -214,13 +263,16 @@ if __name__ == '__main__':
     """コマンドから実行時"""
 
     try:
+        number_of_dirty = 0
+
         # まず、ファイルを全部作る
-        create_all_files(
-            upper_limit_span=4  # 11     # 数字が増えると処理が重くなる
+        create_or_update_all_files(
+            upper_limit_span=5  # 11     # 数字が増えると処理が重くなる
             #upper_limit_span=OUT_OF_UPPER_SPAN + 1
             )
 
         # 次に、スリー・レーツを更新する
+        # depth は upper_limit_coins に対応
         for depth in range(1, 4):      # range(1, 10)
             print(f"search {depth=} ...")
 
