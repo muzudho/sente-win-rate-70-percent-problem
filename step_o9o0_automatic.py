ff6feebad02327a10945d1c80d5cfb4056666ae2
むずでょ <muzudho1@gmail.com>
2024-10-07 15:57:59 +0900

    リナンバリング

--- step_o9o0_automatic.py
+++ step_o9o0_automatic.py
@@ -1,6 +1,5 @@
 #
-# やっつけプログラム３号
-# python step_o3o0_automatic.py
+# python step_o9o0_automatic.py
 #
 #
 
@@ -8,7 +7,7 @@ import traceback
 import datetime
 
 from library import FROZEN_TURN, ALTERNATING_TURN, UPPER_LIMIT_FAILURE_RATE, Converter
-from scripts.step_o3o0_create_kakukin_data_excel_file import Automation as StepO3o0CreateKakukinDataExcelFileAutomation
+from scripts.step_o9o0_create_kakukin_data_excel_file import Automation as StepO9o0CreateKakukinDataExcelFileAutomation
 
 
 ########################################
@@ -30,7 +29,7 @@ if __name__ == '__main__':
                 #
                 #   NOTE 先にKDSファイルを作成しておく必要があります
                 #
-                automation = StepO3o0CreateKakukinDataExcelFileAutomation(
+                automation = StepO9o0CreateKakukinDataExcelFileAutomation(
                         specified_trial_series=2000,
                         specified_turn_system_id=specified_turn_system_id,
                         specified_failure_rate=specified_failure_rate)
--- step_o9o0_manual.py
+++ step_o9o0_manual.py
@@ -1,14 +1,12 @@
 #
-# TODO 作成中
+# python step_o9o0_automatic.py
 #
-# python create_a_kakukin_data_excel_file.py
 #
-# Excel ファイルを作ろう
-#
-import traceback
 
-from library import FROZEN_TURN, ALTERNATING_TURN, Converter
-from library.views import PromptCatalog
+import traceback
+import datetime
+
+from library import Converter, PromptCatalog
 from scripts.step_o9o0_create_kakukin_data_excel_file import Automation as StepO9o0CreateKakukinDataExcelFileAutomation
 
 
@@ -23,18 +21,29 @@ if __name__ == '__main__':
 
         # ［先後の決め方］を尋ねます
         specified_turn_system_id = PromptCatalog.which_method_do_you_use_to_determine_sente_and_gote()
+        turn_system_name = Converter.turn_system_id_to_name(specified_turn_system_id)
 
 
         # ［将棋の引分け率］を尋ねます
         specified_failure_rate = PromptCatalog.what_is_the_failure_rate()
 
 
+
+        print(f"[{datetime.datetime.now()}][turn_system_name={Converter.turn_system_id_to_name(specified_turn_system_id)}  failure_rete={specified_failure_rate * 100:.1f}%] create kakukin data excel file ...")
+
+        # ［かくきんデータ］エクセル・ファイルの作成
+        #
+        #   NOTE 先にKDSファイルを作成しておく必要があります
+        #
         automation = StepO9o0CreateKakukinDataExcelFileAutomation(
                 specified_trial_series=specified_trial_series,
                 specified_turn_system_id=specified_turn_system_id,
                 specified_failure_rate=specified_failure_rate)
 
         automation.execute()
+
+        print(f"""\
+でーきたっ！""")
 
 
     except Exception as err:
