f67a50969b0f431639c331a023f996c3a1ad108d
むずでょ <muzudho1@gmail.com>
2024-10-16 20:22:13 +0900

    connect to parent

--- library/game_tree_view.py
+++ library/game_tree_view.py
@@ -7,12 +7,18 @@ class GameTreeView():
         prev_round_th = round_th - 1
         prev_round_no = prev_round_th - 1
 
-        # 先頭行だけ、ラウンド０に接続できる
-        if prev_round_no == -1:
-            return curr_gt_record.no == 1
+        # 先頭行は、ラウンド０も含め、全部親ノードに接続できる
+        if curr_gt_record.no == 1:
+            return True
 
         try:
-            return prev_gt_record.node_at(prev_round_no) != curr_gt_record.node_at(prev_round_no)
-        
+            # 前ラウンドは、前行とレートが異なるか？
+            return prev_gt_record.node_at(prev_round_no).rate != curr_gt_record.node_at(prev_round_no).rate
+
+        # AttributeError: 'NoneType' object has no attribute 'rate'
+        except AttributeError as e:
+            raise AttributeError(f"{round_th=}  {prev_round_no=}  {prev_gt_record.node_at(prev_round_no)=}  {curr_gt_record.node_at(prev_round_no)=}") from e
+
+        # IndexError: round_th=9  prev_round_no=7  prev_gt_record.len_node_list=6  curr_gt_record.len_node_list=6
         except IndexError as e:
             raise IndexError(f"{round_th=}  {prev_round_no=}  {prev_gt_record.len_node_list=}  {curr_gt_record.len_node_list=}") from e
