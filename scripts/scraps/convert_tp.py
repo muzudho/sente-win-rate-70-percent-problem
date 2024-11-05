4e2ebd511c26a9b7ba4ee66206b9ef64eae7a055
muzudho <muzudho1@gmail.com>
2024-11-05 18:47:08 +0900

    WIP

--- scripts/scraps/convert_tp.py
+++ scripts/scraps/convert_tp.py
@@ -11,7 +11,7 @@ import random
 import math
 import pandas as pd
 
-from library import get_list_of_basename
+from library import round_letro, get_list_of_basename
 from library.file_paths import TheoreticalProbabilityFilePaths
 
 
@@ -37,12 +37,17 @@ def execute():
             df = pd.read_csv(csv_file_path)
 
             if 't_time' not in df.index:
-                df['t_time'] = None
+                # t_time の計算方法は、 span / t_step ※小数点切り上げ
+                a_series = df['span'] / df['t_step']
+                print(f"{type(a_series)=}")
+                # Series クラスの map() 関数は処理が遅いが、独自関数を使える
+                df['t_time'] = a_series.map(round_letro)
 
             if 'h_time' not in df.index:
-                df['h_time'] = None
+                # h_time の計算方法は、 span / h_step ※小数点切り上げ
+                df['h_time'] = (df['span'] / df['h_step']).map(round_letro)
             
-            df.to_csv(csv_file_path)
+            df.to_csv(csv_file_path, index=False)
 
             break
 
